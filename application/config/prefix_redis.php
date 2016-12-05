334fafa514cd3a8c065c0ebdd387552cbd60869c
root <root@iZ94i1sctzeZ.(none)>
2016-12-05 21:54:31 +0800

    add redis prefix

---
+++ application/config/prefix_redis.php
@@ -0,0 +1,11 @@
+<?php
+define('ABC', 'abc');
+define('MY_REDIS_IMAGE', 'redis_image');
+define('MY_REDIS_MATCH_SESSION', 'redis_match_session');
+define('MY_REDIS_MATCH_LIVE_MESSAGE', 'redis_match_live_message');
+define('MY_REDIS_MATCH_LIVE_TICKET', 'redis_match_live_ticket');
+define('MY_REDIS_MATCH_LIVE_STATISTIC', 'redis_match_live_statistic');
+$config['prefix_redis'] = array(
+
+
+);
